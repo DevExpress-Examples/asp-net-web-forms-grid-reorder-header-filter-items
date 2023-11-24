e3e0f32bf9c7b14abbf2e223be8b8ab07c83876a
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2023-11-24 23:38:46 +0400

    Update Default.aspx.cs

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -5,28 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-    protected void ASPxGridView1_HeaderFilterFillItems(object sender, DevExpress.Web.ASPxGridViewHeaderFilterEventArgs e)
-    {
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) { }
+    protected void ASPxGridView1_HeaderFilterFillItems(object sender, DevExpress.Web.ASPxGridViewHeaderFilterEventArgs e) {
         if (e.Column.FieldName == "CategoryName") {
             var orderedList = e.Values.OrderByDescending(x => x.DisplayText, new MyComparer()).ToList();
             e.Values.Clear();
             e.Values.AddRange(orderedList);
         }        
     }
-    public class MyComparer:IComparer<string>
-    {       
-        public MyComparer()
-        {
-            
-        }
-        public int Compare(string x, string y)
-        {            
+    public class MyComparer:IComparer<string> {       
+        public MyComparer() { }
+        public int Compare(string x, string y) {            
             if (x == y)
                 return 0;
             if (x.StartsWith("(") && y.StartsWith("("))
